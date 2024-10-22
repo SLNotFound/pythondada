09d7d15cbe6f597c6ef300ec2fa1ae73bad9f133
sunlei <1255193087@qq.com>
2024-10-22 15:53:16 +0800

    输入输出

---
+++ day02/exercise01.py
@@ -0,0 +1,13 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------
+# ProjectName: pythondada
+# Author: sunlei
+# FileName: exercise01.py
+
+"""
+    汇率转换器
+"""
+usd = float(input("请输入美元："))
+cny = usd * 7.1228
+print(str(usd) + "美元=" + str(cny) + "人民币")
---
+++ day02/exercise03.py
@@ -0,0 +1,17 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------
+# ProjectName: pythondada
+# Author: sunlei
+# FileName: exercise03.py
+"""
+判断英文句子成分：I kiss you
+    效果：
+    请输入I kiss you的主语:I
+    请输入I kiss you的谓语:kiss
+    请输入I kiss you的宾语:you
+"""
+zhuyu = input("请输入I kiss you的主语:")
+weiyu = input("请输入I kiss you的谓语:")
+binyu = input("请输入I kiss you的宾语:")
+print("您输入的主语是:" + zhuyu + "，谓语是:" + weiyu + "，宾语是:" + binyu)
