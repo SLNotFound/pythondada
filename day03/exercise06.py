33b3047b71d64b78bbc67f63cfa1c69bb6672675
sunlei <1255193087@qq.com>
2024-10-23 09:56:01 +0800

    条件语句

---
+++ day03/exercise06.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------
+# ProjectName: pythondada
+# Author: sunlei
+# FileName: exercise06.py
+"""
+判断一个整数的奇偶性
+"""
+number = int(input("请输入一个整数："))
+if number % 2 == 0:
+    print("偶数")
+else:
+    print("奇数")
